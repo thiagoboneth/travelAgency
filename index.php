0d220cd9bde479729aa5135a4b0a02b6844ce9c5
thiagoboneth <thiago.luis@mercadolivre.com>
2022-06-04 13:05:07 -0300

    base completo

--- index.php
+++ index.php
@@ -4,12 +4,30 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/css/bootstrap.min.css">
     <title>Agencia de Turismo</title>
 </head>
 <body>
-    <h1>Teste</h1>
-    <?echo
-        phpinfo();
-    ?>
+    <header class="cabecalho">
+        <h1>Agência de Viagens</h1>
+        <h2>Escolha os melhores destinos</h2>
+    </header>
+    <main class="principal">
+        <div class="conteudo">
+            <div class="modulos">
+                <div class="pacote1">
+                    <ul>
+                        <li><a href="#">"NOME PACOTE</a></li>
+                        <li><a href="#">"DIAS"</a></li>
+                        <li><a href="#">"PRECOS></a></li>
+                        <button>Comprar</button>
+                    </ul>
+                </div>
+            </div>
+        </div>
+    </main>
+    <footer class="rodape">
+        ALUNOS FMF - 2022
+    </footer>
 </body>
 </html>
